aaba69dd261f992f6f4bc3d44588597e654e4c34
Ali <Alitech3@users.noreply.github.com>
2019-10-22 14:36:41 +0200

    Delete marker.js

--- Commands/!Prefixed/marker.js
+++
@@ -1,9 +0,0 @@
-exports.run = (Vargs, voiceChannel, message, ytdl, Discord) => {
-    voiceChannel.join()
-        .then(connection => {
-            const dispatcher = connection.playFile('./Audio/Marker.mp3', { seek: 0, volume: 2.5/10});
-
-            dispatcher.on('end', () => voiceChannel.leave());
-
-        }).catch(console.error);
-};
